5388b3e22a5603f5a700f03e7e0432bd299a75ec
Brant McCarthy <brant.mccarthy@me.com>
2022-03-09 14:25:45 -0600

    added server post

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,7 +6,7 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  pool.query(`SELECT * FROM "item";`).then(result => res.sendStatus(200))
+  pool.query(`SELECT * FROM "item";`).then(result => res.send(result.rows))
   .catch(err => {
     console.log('ERROR in GET', err);
     res.sendStatus(500);
@@ -17,6 +17,18 @@ router.get('/', (req, res) => {
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
+  const shelfItem = req.body;
+  const userId = req.user.id;
+  const queryText = `INSERT INTO "item" (description, image_url, user_id)
+  VALUES ($1, $2, $3);`;
+  pool.query(queryText, [shelfItem.description, shelfItem.image_url, userId])
+  .then(result => {
+    console.log('in POST .then');
+    res.sendStatus(200)
+  }).catch(err => {
+    console.log('ERROR in POST', err);
+    res.sendStatus(500)
+  })
   // endpoint functionality
 });
 
